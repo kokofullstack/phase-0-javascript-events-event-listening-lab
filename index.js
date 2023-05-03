2f40debfd6072a6ac4c50ca945ae5b1a8eea74d5
Maung Ko Ko <mwkoko2020@gmail.com>
2023-05-03 13:19:47 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+      // Code to run when the button is clicked
+    });
+  }
+  
